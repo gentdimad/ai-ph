727637379d1cc9625097ea1ba5623358ae2d01df
gentdimad <gentdimad@gmail.com>
2026-02-24 16:19:50 +0800

    modified Page component formatting

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -7,7 +7,7 @@ export default function Page({ children, fluid = false }: { children: React.Reac
     <div className="page">
       {/*<Header/>*/}
       <Shell>
-        <main className="py-20 px-4 sm:px-8 md:px-12 lg:px-16">
+        <main className="px-4 sm:px-8 md:px-12 lg:px-16">
           <Container className={fluid ? 'max-w-none' : ''}>
             {children}
           </Container>
